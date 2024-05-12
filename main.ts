e71c5597f61f1dd100e72db95e2e0412b5694341
ofrich14 <ofrich.home@gmail.com>
2024-05-12 17:37:00 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,44 @@
-input.onGesture(Gesture.LogoUp, function () {
-    radio.sendString("f")
+input.onGesture(Gesture.ScreenUp, function () {
+    radio.sendString("b")
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
 })
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendString("r")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    radio.sendString("f")
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # . . .
+        `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     radio.sendString("l")
-})
-input.onGesture(Gesture.LogoDown, function () {
-    radio.sendString("b")
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # # # .
         `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
 })
