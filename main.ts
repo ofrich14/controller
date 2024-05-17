2850441dd218e75187c0e67779abeeae0d1da291
ofrich14 <ofrich.home@gmail.com>
2024-05-17 20:08:39 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -34,7 +34,43 @@ input.onGesture(Gesture.ScreenDown, function () {
     music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("c")
+    basic.showLeds(`
+        . # # # .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # # # .
+        `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
+    basic.clearScreen()
+})
 input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendString("r")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("r")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        `)
+    music.play(music.stringPlayable("C5 B A G F E D C ", 333), music.PlaybackMode.UntilDone)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
     radio.sendString("r")
     basic.showLeds(`
         . . . . .
